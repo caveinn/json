cb888d8985745ccf1d59433613b7a3795c28a8bb
caveinn <caveinncicad@gmail.com>
2021-02-18 00:35:55 +0300

    clean project

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ navigator_column = [
     [sg.Text("Application Running for:")],
     [sg.Text("Last Delivered campaign")],
     [sg.Text("Last Sync Campaign JSON")],
-    [sg.Text("Last Sync /Ads folder")],
-    [sg.T("", (40,50))],
+    [sg.Text("Last Sync /Ads folder", pad=((6,400),(0,400) ))],
     [sg.Text("Sart /stop services:"), sg.Button("stop")],
-    [sg.Text("Automatically start at login"), sg.Checkbox("")],
+    [sg.Text("Automatically start at login"), sg.Checkbox("", key="-autostart-", enable_events=True)],
     # [sg.Text(size=(40, 1), key="-TOUT-")],
     # [sg.Image(key="-IMAGE-")],
 
@@ -31,19 +30,18 @@ layout = [
         sg.Column(info_column,vertical_alignment="t"),
         sg.VSeperator(),
         sg.Column(navigator_column),
-
-
     ]
 ]
 
 # Create the window
-window = sg.Window("Support App", layout, resizable=True)
+window = sg.Window("Support App", layout)
 
 # Create an event loop
 while True:
     event, values = window.read()
     # End program if user closes window or
     # presses the OK button
+    print(event, values)
     if event == "OK" or event == sg.WIN_CLOSED:
         break
 
